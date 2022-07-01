869058383e16e46e19643746918eb3b0d83b7235
Dylan <dylan.vonwald@gmail.com>
2022-06-30 22:08:51 -0600

    trying to get the user routes working

--- models/Reaction.js
+++ models/Reaction.js
@@ -21,3 +21,5 @@ const reactionSchema = new Schema({
 });
 
 //this will not be a model but rather will be used as the reaction field's subdocument schema in the Thought model
+
+module.exports = Reaction;
--- models/User.js
+++ models/User.js
@@ -1,39 +1,44 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    //must match a valid email address
-  },
-  // thoughts: array of _id values referencing the Thought model,
-  thoughts: [
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //must match a valid email address
+    },
+    // thoughts: array of _id values referencing the Thought model,
+    thoughts: [
       {
         type: Schema.Types.ObjectID,
-        ref: 'thought',
-      }
-  ],
-  // friends: array of _id values referencing the User model (self-reference)
-  friends: [
+        ref: "thought",
+      },
+    ],
+    // friends: array of _id values referencing the User model (self-reference)
+    friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'friends'
-      }
-  ],
+        ref: "friends",
+      },
+    ],
+  },
   {
-      toJSON: {
-          virtuals: true,
-      }
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
   }
-});
+);
 
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
+
+module.exports = User;
